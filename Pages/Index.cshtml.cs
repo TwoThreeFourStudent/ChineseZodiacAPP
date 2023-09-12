123358c291d4d7d1ea14a2100cc2d404aad6e586
Miguel Magpoc <mmagpoc@my.bcit.ca>
2023-09-12 13:50:44 -0700

    first commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ChineseWebAPP.Models;
+
+public class IndexModel : PageModel
+{
+    [BindProperty]
+    public int Year { get; set; }
+
+    [TempData]
+    public string ZodiacSign { get; set; }
+
+    public IActionResult OnPost()
+    {
+        if (ModelState.IsValid)
+        {
+            ZodiacSign = ZodiacUtils.GetZodiac(Year).ToLower();
+        }
+        return Page();
+    }
+}
